58cf5228076e294cc1adf8897dea351585296f54
João Victor <joaodddev@gmail.com>
2026-03-14 14:14:44 -0300

    Update LoginRequestDTO.java

--- auth-api/authapi/src/main/java/com/joaodddev/authapi/dto/LoginRequestDTO.java
+++ auth-api/authapi/src/main/java/com/joaodddev/authapi/dto/LoginRequestDTO.java
@@ -7,11 +7,11 @@ import lombok.Data;
 
 @Data
 public class LoginRequestDTO {
-
+    
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Email inválido")
     private String email;
-
+    
     @NotBlank(message = "Senha é obrigatória")
     @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
     private String password;
