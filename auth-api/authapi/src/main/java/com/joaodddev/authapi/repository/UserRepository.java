a6366c1a8207dec6da10b0e770a733726c6b4667
João Victor <joaodddev@gmail.com>
2026-03-14 14:14:04 -0300

    Update UserRepository.java

--- auth-api/authapi/src/main/java/com/joaodddev/authapi/repository/UserRepository.java
+++ auth-api/authapi/src/main/java/com/joaodddev/authapi/repository/UserRepository.java
@@ -8,8 +8,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-
     boolean existsByEmail(String email);
-
-    Optional<User> findByPhone(String phone);
 }
