30cd3736cff990856856d1201d76fdc9ce77980f
João Victor <joaodddev@gmail.com>
2026-03-14 14:16:39 -0300

    Update CustomUserDetailsService.java

--- auth-api/authapi/src/main/java/com/joaodddev/authapi/service/CustomUserDetailsService.java
+++ auth-api/authapi/src/main/java/com/joaodddev/authapi/service/CustomUserDetailsService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-
+    
     private final UserRepository userRepository;
-
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
