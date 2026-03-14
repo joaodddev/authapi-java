79c401989d554f483c1ab152c9ff8842c237a5c3
João Victor <joaodddev@gmail.com>
2026-03-14 14:15:22 -0300

    Update RegisterRequestDTO.java

--- auth-api/authapi/src/main/java/com/joaodddev/authapi/dto/RegisterRequestDTO.java
+++ auth-api/authapi/src/main/java/com/joaodddev/authapi/dto/RegisterRequestDTO.java
@@ -8,19 +8,19 @@ import lombok.Data;
 
 @Data
 public class RegisterRequestDTO {
-
+    
     @NotBlank(message = "Nome completo é obrigatório")
     @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     private String fullName;
-
+    
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Email inválido")
     private String email;
-
+    
     @NotBlank(message = "Senha é obrigatória")
     @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
     private String password;
-
+    
     @Pattern(regexp = "^\\d{10,11}$", message = "Telefone deve ter 10 ou 11 dígitos")
     private String phone;
 }
